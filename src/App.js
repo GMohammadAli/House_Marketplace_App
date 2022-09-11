f395022ca3835bf70fa7c103c99febf243d93f6c
GMohammadAli <gulammohammadali@gmail.com>
2022-09-11 18:22:00 +0530

    Contact Landlord Page Done

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Profile from "./pages/Profile";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
 import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -28,7 +29,14 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
-          <Route path="/category/:categoryName/:listingId" element={<Listing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
+          <Route
+            path="/contact/:landlordId"
+            element={<Contact />}
+          />
         </Routes>
         <Navbar />
 
