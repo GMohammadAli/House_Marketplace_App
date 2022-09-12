557febcea4a84af95bb2fa46be18b186bee46c71
GMohammadAli <gulammohammadali@gmail.com>
2022-09-12 13:39:51 +0530

    Recommended Slider Added

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,6 +1,7 @@
 import {Link} from "react-router-dom"
 import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg"
 import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg"
+import Slider from "../components/Slider";
 
 function Explore() {
   return (
@@ -11,6 +12,7 @@ function Explore() {
 
       <main>
         {/* Slider */}
+        <Slider />
 
         <p className="exploreCategoryHeading">Categories</p>
         <div className="exploreCategories">
